3238f4634661fd3cfe8cfb14f471a3ca6ffed88c
İbrahim Hilmi Akargül <ibrahim.akargul+atlassian.com@gmail.com>
2023-05-05 08:41:37 +0300

    init

---
+++ src/test/java/com/apibinder/sweeter/SweeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apibinder.sweeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
